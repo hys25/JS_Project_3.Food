23da09427c58ffef67f6994b52e1b28b301fe23e
Halyna Yavtushenko <galya.yavtushenko@gmail.com>
2020-07-28 11:41:37 +0300

    favicon

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ window.addEventListener('DOMContentLoaded', function() {
 
     tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
     modal('[data-modal]', '.modal', modalTimerId);
-    timer('.timer', '2020-11-10');
+    timer('.timer', '2020-10-10');
     cards();
     calc();
     forms('form', modalTimerId);
